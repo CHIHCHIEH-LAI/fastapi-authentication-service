6aca3701ef73d197eeb3c7d8abfb79c36b497fbf
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-18 22:55:59 +0800

    Refactor CRUD class in crud.py for better code organization

--- app/crud.py
+++ app/crud.py
@@ -1,27 +1,32 @@
 from pymysql.connections import Connection
 
-def create_account(db: Connection, username: str, password: str) -> bool:
-    with db.cursor() as cursor:
-        # Check if the username already exists
-        cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
-        if cursor.fetchone():
-            return False # Username already exists
-        
-        # Create the account
-        cursor.execute("INSERT INTO accounts (username, password) VALUES (%s, %s)", (username, password))
-        db.commit()
-    return True
+class CRUD:
 
-def verify_account(db: Connection, username: str, password: str) -> bool:
-    with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
+    def __init__(self, db: Connection):
+        self.db = db
 
-        result = cursor.fetchone()
+    def create_account(self, username: str, password: str) -> bool:
+        with self.db.cursor() as cursor:
+            # Check if the username already exists
+            cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
+            if cursor.fetchone():
+                return False # Username already exists
+            
+            # Create the account
+            cursor.execute("INSERT INTO accounts (username, password) VALUES (%s, %s)", (username, password))
+            db.commit()
+        return True
 
-        if not result:
-            return (False, False) # Username doesn't exist
-        
-        if result['password'] != password:
-            return (True, False) # Password is incorrect
+    def verify_account(self, username: str, password: str) -> bool:
+        with self.db.cursor() as cursor:
+            cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
 
-        return (True, True)
+            result = cursor.fetchone()
+
+            if not result:
+                return (False, False) # Username doesn't exist
+            
+            if result['password'] != password:
+                return (True, False) # Password is incorrect
+
+            return (True, True)
