1b3ab47c68a89fceb04b089698abef61ecaa0611
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-18 21:54:04 +0800

    Refactor database connection initialization in database.py

--- app/database.py
+++ app/database.py
@@ -1,13 +1,17 @@
 import pymysql
 from pymysql.cursors import DictCursor
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Database:
-    def __init__(self, host='localhost', user='root', password='', db='test'):
+    def __init__(self):
         self.connection = None
-        self.host = host
-        self.user = user
-        self.password = password
-        self.db = db
+        self.host = os.getenv('MYSQL_HOST', 'localhost')
+        self.user = os.getenv('MYSQL_USER', 'root')
+        self.password = os.getenv('MYSQL_PASSWORD', '')
+        self.db = os.getenv('MYSQL_DB', 'test')
 
     def connect(self):
         if not self.connection or not self.connection.open:
