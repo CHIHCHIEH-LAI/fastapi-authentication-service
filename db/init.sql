f427e78241096c6eb84599671631f78f482abd76
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-18 23:39:54 +0800

    Add new columns to accounts table in init.sql

--- db/init.sql
+++ db/init.sql
@@ -4,5 +4,7 @@ USE accountDB;
 CREATE TABLE IF NOT EXISTS accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(32) UNIQUE NOT NULL,
-    password VARCHAR(32) NOT NULL
+    password VARCHAR(32) NOT NULL,
+    failed_attempts INT DEFAULT 0,
+    last_login TIMESTAMP DEFAULT NULL
 );
