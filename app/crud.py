6f008370bf7b4f19d45214bc748aee408e3b90d4
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-19 00:25:49 +0800

    Refactor CRUD class in crud.py for better code organization

--- app/crud.py
+++ app/crud.py
@@ -14,19 +14,11 @@ class CRUD:
             
             # Create the account
             cursor.execute("INSERT INTO accounts (username, password) VALUES (%s, %s)", (username, password))
-            db.commit()
+            self.db.commit()
         return True
 
-    def verify_account(self, username: str, password: str) -> bool:
+    def read_account(self, username: str, password: str) -> bool:
         with self.db.cursor() as cursor:
             cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
 
-            result = cursor.fetchone()
-
-            if not result:
-                return (False, False) # Username doesn't exist
-            
-            if result['password'] != password:
-                return (True, False) # Password is incorrect
-
-            return (True, True)
+            return cursor.fetchone()
