0f051e5eb50911ae3c03b9bc5007b8c1038bcbe5
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-19 00:42:47 +0800

    include update_failed_attempts and read_account methods

--- app/crud.py
+++ app/crud.py
@@ -17,8 +17,12 @@ class CRUD:
             self.db.commit()
         return True
 
-    def read_account(self, username: str, password: str) -> bool:
+    def read_account(self, username: str) -> bool:
         with self.db.cursor() as cursor:
             cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
-
             return cursor.fetchone()
+        
+    def update_failed_attempts(self, username: str, failed_attempts: int, current_time) -> None:
+        with self.db.cursor() as cursor:
+            cursor.execute("UPDATE accounts SET failed_attempts = %s, last_attempt_time = %s WHERE username = %s", (failed_attempts, current_time, username))
+            self.db.commit()
