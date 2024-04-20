f5a088a58af71e0182609a04466341514ce422f6
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-21 01:20:49 +0800

    Refactor error messages in routes.py to use JSONResponse

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
 from datetime import datetime
 from app.model import Account
 from app.dependencies import get_db
@@ -12,7 +13,7 @@ app = FastAPI()
 async def create_account(account: Account):
     crud = CRUD(db)
     if not crud.create_account(account.username, account.password):
-        raise HTTPException(status_code=400, detail={"success": False, "reason": "Username already exists"})
+        return JSONResponse(status_code=400, content={"success": False, "reason": "Username already exists"})
     
     return {"success": True, "reason": "Account created"}
 
@@ -22,17 +23,17 @@ async def verify_account(account: Account):
     result = crud.read_account(account.username)
     
     if not result:
-        raise HTTPException(status_code=404, detail={"success": False, "reason": "Username not found"})
+        return JSONResponse(status_code=404, content={"success": False, "reason": "Username not found"})
     
     current_time = datetime.now().replace(microsecond=0)
-    failed_attemps = result['failed_attempts']
+    failed_attempts = result['failed_attempts']
 
-    if failed_attemps >= 5 and result['last_attempt_time'] and (current_time - result['last_attempt_time']).seconds < 60:
-        raise HTTPException(status_code=429, detail={"success": False, "reason": "Too many failed attempts. Try again in 60 seconds"})
+    if failed_attempts >= 5 and result['last_attempt_time'] and (current_time - result['last_attempt_time']).seconds < 60:
+        return JSONResponse(status_code=429, content={"success": False, "reason": "Too many failed attempts. Try again in 60 seconds"})
     
     if result['password'] != account.password:
-        crud.update_verify_status(account.username, failed_attemps+1, current_time)
-        raise HTTPException(status_code=401, detail={"success": False, "reason": "Invalid password"})
+        crud.update_verify_status(account.username, failed_attempts+1, current_time)
+        return JSONResponse(status_code=401, content={"success": False, "reason": "Invalid password"})
     
     crud.update_verify_status(account.username, 0, current_time)
     return {"success": True, "reason": "Account verified"}
