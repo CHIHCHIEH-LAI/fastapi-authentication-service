0daaf7941e12abd57797567096c0649e60f00b48
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-21 01:10:44 +0800

    Refactor error messages in routes.py to include success status and reason

--- app/routes.py
+++ app/routes.py
@@ -12,9 +12,9 @@ app = FastAPI()
 async def create_account(account: Account):
     crud = CRUD(db)
     if not crud.create_account(account.username, account.password):
-        raise HTTPException(status_code=400, detail="Username already exists")
+        raise HTTPException(status_code=400, detail={"success": False, "reason": "Username already exists"})
     
-    return {"message": "Account created"}
+    return {"success": True, "reason": "Account created"}
 
 @app.post("/verify_account", status_code=200)
 async def verify_account(account: Account):
@@ -22,17 +22,17 @@ async def verify_account(account: Account):
     result = crud.read_account(account.username)
     
     if not result:
-        raise HTTPException(status_code=404, detail="Username not found")
+        raise HTTPException(status_code=404, detail={"success": False, "reason": "Username not found"})
     
     current_time = datetime.now().replace(microsecond=0)
     failed_attemps = result['failed_attempts']
 
     if failed_attemps >= 5 and result['last_attempt_time'] and (current_time - result['last_attempt_time']).seconds < 60:
-        raise HTTPException(status_code=429, detail="Too many failed attempts. Please wait for one minute before trying again.")
+        raise HTTPException(status_code=429, detail={"success": False, "reason": "Too many failed attempts. Try again in 60 seconds"})
     
     if result['password'] != account.password:
         crud.update_verify_status(account.username, failed_attemps+1, current_time)
-        raise HTTPException(status_code=401, detail="Invalid password")
+        raise HTTPException(status_code=401, detail={"success": False, "reason": "Invalid password"})
     
     crud.update_verify_status(account.username, 0, current_time)
-    return {"message": "Account verified"}
+    return {"success": True, "reason": "Account verified"}
