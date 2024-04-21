64635dd9eec4ae5d693aab2345ca46fd0b028338
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-21 09:58:50 +0800

    Refactor error messages in routes.py to use status codes and JSONResponse

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from fastapi.responses import JSONResponse
 from datetime import datetime
 from app.model import Account
@@ -9,31 +9,31 @@ db = get_db()
 
 app = FastAPI()
 
-@app.post("/create_account", status_code=201)
+@app.post("/create_account", status_code=status.HTTP_201_CREATED)
 async def create_account(account: Account):
     crud = CRUD(db)
     if not crud.create_account(account.username, account.password):
-        return JSONResponse(status_code=400, content={"success": False, "reason": "Username already exists"})
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content={"success": False, "reason": "Username already exists"})
     
-    return {"success": True, "reason": "Account created"}
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"success": True, "reason": "Account created"})
 
-@app.post("/verify_account", status_code=200)
+@app.post("/verify_account", status_code=status.HTTP_200_OK)
 async def verify_account(account: Account):
     crud = CRUD(db)
     result = crud.read_account(account.username)
     
     if not result:
-        return JSONResponse(status_code=404, content={"success": False, "reason": "Username not found"})
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"success": False, "reason": "Username not found"})
     
     current_time = datetime.now().replace(microsecond=0)
     failed_attempts = result['failed_attempts']
 
     if failed_attempts >= 5 and result['last_attempt_time'] and (current_time - result['last_attempt_time']).seconds < 60:
-        return JSONResponse(status_code=429, content={"success": False, "reason": "Too many failed attempts. Try again in 60 seconds"})
+        return JSONResponse(status_code=status.HTTP_429_TOO_MANY_REQUESTS, content={"success": False, "reason": "Too many failed attempts. Try again in 60 seconds"})
     
     if result['password'] != account.password:
         crud.update_verify_status(account.username, failed_attempts+1, current_time)
-        return JSONResponse(status_code=401, content={"success": False, "reason": "Invalid password"})
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"success": False, "reason": "Invalid password"})
     
     crud.update_verify_status(account.username, 0, current_time)
-    return {"success": True, "reason": "Account verified"}
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"success": True, "reason": "Account verified"})
